84dfba12dc40f77ed8e8cfbcbc504d0400708006
Narayani <bluenetemp4@outlook.com>
2025-06-25 13:36:31 +0530

    Booking Form and Mobile  View

--- src/App.js
+++ src/App.js
@@ -26,6 +26,21 @@ const Navbar = () => (
     </ul>
   </nav>
 );
+const responsive = {
+
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 14
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+};
 
 const Home = () => (
   <section id="home" className="home">
